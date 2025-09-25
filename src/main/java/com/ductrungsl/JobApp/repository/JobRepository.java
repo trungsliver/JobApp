d4e4d1fc7248343467b4e652d7fcdf17462895c8
trungsliver <buiductrung.cva@gmail.com>
2025-09-25 16:33:59 +0700

    job repository

---
+++ src/main/java/com/ductrungsl/JobApp/repository/JobRepository.java
@@ -0,0 +1,12 @@
+package com.ductrungsl.JobApp.repository;
+
+import com.ductrungsl.JobApp.entity.Job;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobRepository {
+    List<Job> findAll();
+    void addJob(Job job);
+}
