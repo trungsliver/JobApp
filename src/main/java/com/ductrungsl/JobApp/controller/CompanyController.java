0c9949a30f5b2346c1f8ba2f3ed49f6e1ac0f04e
trungsliver <buiductrung.cva@gmail.com>
2025-10-02 14:52:32 +0700

    controller get all companies

--- src/main/java/com/ductrungsl/JobApp/controller/CompanyController.java
+++ src/main/java/com/ductrungsl/JobApp/controller/CompanyController.java
@@ -1,4 +1,30 @@
 package com.ductrungsl.JobApp.controller;
 
+import com.ductrungsl.JobApp.entity.Company;
+import com.ductrungsl.JobApp.service.CompanyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+@RequiredArgsConstructor
 public class CompanyController {
+    @Autowired
+    private CompanyService companyService;
+
+    @GetMapping
+    public ResponseEntity<List<Company>> getAllCompanies() {
+        if (companyService.findAll().isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(companyService.findAll(), HttpStatus.OK);
+    }
+
 }
